67f978b37aee1e7c4ff34929845d9d7709314e02
sivilela <sivilela.s@gmail.com>
2023-05-30 21:08:37 -0300

    Exercícios de Laços I pronto

---
+++ exercicio3/script.js
@@ -0,0 +1,25 @@
+/**Exercício 3
+
+Crie um array com 5 strings. Faça um programa que percorra este array e imprima 
+as strings em formato de ranking, como no exemplo abaixo:
+
+//entrada
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+
+//saída
+"1 - Rússia"
+"2 - Canadá"
+"3 - China"
+"4 - EUA"
+"5 - Brasil" */
+
+function pais (){
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+
+for(let i = 0; i < maioresPaises.length; i++){
+    console.log(`${i} - ${maioresPaises[i]}`);
+}
+}
+//pais()
+
+
